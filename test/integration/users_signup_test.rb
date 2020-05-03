20683b0ab905f5c8f174ffcbda0182640aaf7cc6
EC2 Default User <ec2-user@ip-172-31-88-75.ec2.internal>
2020-05-03 06:40:19 +0000

    add signup route and test and view

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  
+  test "invalid signup" do
+    get signup_path
+    assert_no_difference "User.count" do
+      post users_path params: {user: {name:"",
+                                    password:"",
+                                    password_confirmation:"",
+                                    prefecture:"東京都"}}
+      end
+    assert_template "users/new"
+    assert_select "div#error_explanation"
+  end
+  
+  test "valid signup" do
+    get signup_path
+    assert_difference "User.count", 1 do
+      post users_path params: {user: {name:"rails",
+                                    password:"password",
+                                    password_confirmation:"password",
+                                    prefecture:""}}
+      end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+end
