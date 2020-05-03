057526e69c4d7460796e480e249afebd8d15e82e
EC2 Default User <ec2-user@ip-172-31-88-75.ec2.internal>
2020-05-03 15:55:26 +0000

    post-controler bug is correct

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
    
    validates :name, 
    presence: true,
-   length: { maximum: 20 },
-   uniqueness: true
+   length: { maximum: 20 }
+   #uniqueness: true
    
    has_secure_password
    validates :password,presence: true, length: { minimum: 6 },allow_nil: true
