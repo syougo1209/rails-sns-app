3dbc35769c71c7ad33f52f2303a1ad246e0fc7b9
EC2 Default User <ec2-user@ip-172-31-88-75.ec2.internal>
2020-05-05 13:33:39 +0000

    add initial user picture

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -13,7 +13,12 @@ version :show do
  #version :thumb do
  # process resize_to_fit: [20, 20]
 # end
- 
+  def default_url(*args)
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    "default.jpg"
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  end
  
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
