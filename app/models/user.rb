6b846dab0c2cebf06275dc929b6c91e8f7a46736
EC2 Default User <ec2-user@ip-172-31-88-75.ec2.internal>
2020-07-19 10:30:52 +0000

    homeのUIを整えた

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
    has_many :active_relationships,class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
-  has_many :following, through: :active_relationships, source: :followed
+   has_many :following, through: :active_relationships, source: :followed
     has_many :passive_relationships, class_name:  "Relationship",
                                    foreign_key: "followed_id",
                                    dependent:   :destroy
